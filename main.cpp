98f4b4d15be31f6190035eddd5ef900f5a1f02d3
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-08-25 18:22:56 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
-
+#include <vector>
+#include <string>
+#include <map>
+#include <stdio.h>
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    return 0;
+int main() {
+    int valid = 0;
+    char letras[1000];
+    while(gets(letras)){
+        for(int i = 0; letras[i]; i++) {
+            if(letras[i]=='"') {
+                if (valid == 0 ) {
+                    cout << "``";
+                    valid++;
+                } else if (valid == 1) {
+                    cout << "''";
+                    valid =0;
+                }
+            }
+            else{
+                cout << letras[i];
+            }
+
+        }
+        puts("");
+    }
 }
